20e579b7e5fd61299227cac4e6668c0ec8411973
juliuss28 <juliuss03@outlook.com>
2024-03-20 20:40:31 +0300

    починила список товаров!

--- app/src/main/java/com/example/ecommercecoursework/MainActivity.kt
+++ app/src/main/java/com/example/ecommercecoursework/MainActivity.kt
@@ -4,15 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.ecommercecoursework.model.Product
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+
+        val products = arrayListOf<Product>()
+        for (i in 0..100) {
+            products.add(Product(title = "Organic Apple", photoUrl = "", price = 1.99))
+
+        }
+
         recyclerView.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
-            adapter = ProductsAdapter()
+            adapter = ProductsAdapter(products)
         }
     }
 }
--- app/src/main/java/com/example/ecommercecoursework/ProductsAdapter.kt
+++ app/src/main/java/com/example/ecommercecoursework/ProductsAdapter.kt
@@ -1,26 +1,49 @@
 package com.example.ecommercecoursework
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.ecommercecoursework.ProductsAdapter.*
 import com.example.ecommercecoursework.model.Product
 
-class ProductsAdapter(private val products: ArrayList<Product>) : RecyclerView.Adapter<ViewHolder>() {
+class ProductsAdapter(private val products: ArrayList<Product>) :
+    RecyclerView.Adapter<ProductsAdapter.ViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.product_row, parent, false )
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ProductsAdapter.ViewHolder, position: Int) {
+        holder.title.text = products[position].title
     }
 
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun getItemCount() = products.size
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val image: ImageView = itemView.findViewById(R.id.photo)
+        val title: TextView = itemView.findViewById(R.id.title)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
-
-
-    class ViewHolder : RecyclerView.ViewHolder(){
-
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
